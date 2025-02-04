ecd0f66109e466361ed7afedd1ff646ea911473b
Gael <HadriaxDev@protonmail.com>
2025-02-04 23:53:26 +0100

    Commited todo

--- data/characteristics/injury.py
+++ data/characteristics/injury.py
@@ -12,7 +12,7 @@ class InjuryCharacteristic(Characteristic):
         :raise FileNotFoundError: If the file muscles.json doesn't exist in the source folder.
         """
 
-        #  @todo Handle JSON format errors.
+        # @todo Handle JSON format errors.
         try:
             muscles_df = read_json('source/muscles.json')
         except FileNotFoundError:
